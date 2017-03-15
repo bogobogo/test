fe818da93195ddfde56ee7dc74df4f95aace3245
Elad Bogomolny <eladbo@wix.com>
2017-03-15 10:38:18 +0200

    testing webscrap

--- scripts/webscrap.js
+++ scripts/webscrap.js
@@ -1,8 +1,8 @@
-import fetch from 'node-fetch';
+var fetch = require('node-fetch');
 
-projectList = ['1']
+projectList = ['1'];
 projectList.forEach(function(project) {
     fetch('https://github.com/')
     .then(res => res.text())
     .then(body => console.log(body))
-})
+});
