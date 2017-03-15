4a9f42c43649c48d798b54ef23fc1fb7f15f4209
Elad Bogomolny <eladbo@wix.com>
2017-03-15 10:43:46 +0200

    webscrap changes

--- scripts/webscrap.js
+++ scripts/webscrap.js
@@ -1,8 +1,8 @@
 var fetch = require('node-fetch');
 
-projectList = ['1'];
+projectList = ['https://github.com/bogobogo/opensource2/blob/master/openSourceIsAwesome.json'];
 projectList.forEach(function(project) {
-    fetch('https://github.com/')
-    .then(res => res.text())
-    .then(body => console.log(body))
+    console.log(project);
+    fetch('https://raw.githubusercontent.com/bogobogo/opensource2/master/openSourceIsAwesome.json')
+    .then(res => console.log(res))
 });
