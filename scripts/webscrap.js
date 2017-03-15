87827891bee3aeb9d15937d0bba59b3f7b28dc17
Elad Bogomolny <eladbo@wix.com>
2017-03-15 10:48:00 +0200

    third webscrap attempt with request

--- scripts/webscrap.js
+++ scripts/webscrap.js
@@ -1,8 +1,22 @@
-var fetch = require('node-fetch');
+// var fetch = require('node-fetch');
 
-projectList = ['https://github.com/bogobogo/opensource2/blob/master/openSourceIsAwesome.json'];
-projectList.forEach(function(project) {
-    console.log(project);
-    fetch('https://raw.githubusercontent.com/bogobogo/opensource2/master/openSourceIsAwesome.json')
-    .then(res => console.log(res))
-});
+// projectList = ['https://github.com/bogobogo/opensource2/blob/master/openSourceIsAwesome.json'];
+// projectList.forEach(function(project) {
+//     console.log(project);
+//     fetch('https://raw.githubusercontent.com/bogobogo/opensource2/master/openSourceIsAwesome.json')
+//     .then(res => console.log(res))
+// });
+
+var request = require("request")
+
+var url = "https://raw.githubusercontent.com/bogobogo/opensource2/master/openSourceIsAwesome.json"
+
+request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        console.log(body) // Print the json response
+    }
+})
